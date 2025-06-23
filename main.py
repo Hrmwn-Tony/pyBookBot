341117cf72ce066288a11aac461a5ad7cdf0fcdd
ubuntu-victus <hermawantony.personal@gmail.com>
2025-06-23 16:58:19 +0700

    adding sys.argv, removing hardcoded address, HOWTO : python3 main.py <book-address-path>

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from stats import word_counter, char_counter, sort_dictionary, get_report
+import sys
+
+def checkSys():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
 def main():
-    book = get_book_text("books/frankenstein.txt")
+    address = sys.argv[1]
+    book = get_book_text(f"{address}")
     print(f"{word_counter(book)} words found in the document")
     charDict = char_counter(book)
-    get_report(sort_dictionary(charDict), "books/frankenstein.txt", book)
+    get_report(sort_dictionary(charDict), f"{address}", book)
 
 
     
@@ -14,5 +23,5 @@ def get_book_text(address):
         bookContent = f.read()
     return bookContent
 
-
+checkSys()
 main()
